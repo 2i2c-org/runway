fdc243f76f2fa4510ce4b9e1304d0c95adf3dd3f
choldgraf <choldgraf@gmail.com>
2026-03-18 13:28:48 -0700

    Remove test suite to inline tests instead

--- noxfile.py
+++ noxfile.py
@@ -5,29 +5,7 @@ nox.options.reuse_existing_virtualenvs = True
 
 
 @nox.session
-def download(session):
-    """Download latest deals from HubSpot."""
+def sync(session):
+    """Download, clean, and model - the full pipeline."""
     session.install("-r", "requirements.txt")
-    session.run("python", "scripts/download_data.py")
-
-
-@nox.session
-def test(session):
-    """Run tests on downloaded data."""
-    session.install("-r", "requirements.txt")
-    session.run("pytest", "tests/", "-v")
-
-
-@nox.session
-def update(session):
-    """Run tests, then upload downloaded deals and KPI MAU table."""
-    session.install("-r", "requirements.txt")
-    session.run("pytest", "tests/", "-v")
-    session.run("python", "scripts/upload_data.py")
-
-
-@nox.session(name="download-and-update")
-def download_and_update(session):
-    """Download, test, and upload in sequence."""
-    session.notify("download")
-    session.notify("update")
+    session.run("python", "scripts/sync.py")
